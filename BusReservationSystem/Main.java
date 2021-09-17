fd8f9762fefe66b22bf5f80cae4cda951bf0aeaf
Annie1388 <lewan-wm20@student.tarc.edu.my>
2021-09-17 13:43:42 +0800

    eroor

--- BusReservationSystem/Main.java
+++ BusReservationSystem/Main.java
@@ -723,7 +723,7 @@ public class Main {
                             System.out.println("Invalid input, please enter again");
                     }
                     if(transactionFailed){
-                        System.out.println("Please try again or select other payment methods to complete your payment.")
+                        System.out.println("Please try again or select other payment methods to complete your payment.");
                     }
                 }while (paymentMethod != 1 && paymentMethod != 2 && paymentMethod != 3 && paymentMethod != 4 || transactionFailed);
                 continueInput = false;
